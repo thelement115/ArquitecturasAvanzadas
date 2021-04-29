3880c7e81bb0425c6fbd4bc921dec2d53ec53c95
eduard damian garcia <yamiyugi43@gmail.com>
2021-04-28 21:45:03 -0500

    log in

--- evergreen/authenthication/views.py
+++ evergreen/authenthication/views.py
@@ -6,6 +6,8 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
 
 from django.contrib.auth.models import User
 from . import serializers
@@ -23,10 +25,19 @@ class LoginViewSet(viewsets.ViewSet):
     """ check username and password and return an auth token """
 
     serializer_class = AuthTokenSerializer
-    
+
     def create(self, request):
         serializer = AuthTokenSerializer(data=request.data)
         if serializer.is_valid():
-            return ObtainAuthToken().post(request)
+            user = authenticate(username=request.data['username'], password=request.data['password'])
+            if user is not None:
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                    'token': token.key,
+                    'user_id': user.pk,
+                    'email': user.email
+                })
+            else:
+                return Response({"status_code": 405, "detail": "UNAUTHORIZED"})
         else:
             return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
